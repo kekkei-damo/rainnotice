2d247f6f5b5d89fea2d135afdc3a00d4c683b735
okamotokeita <nong41j@gmail.com>
2020-06-16 20:23:40 +0900

    make it better

---
+++ config/application.rb
@@ -0,0 +1,18 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Rainnotice
+  class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 6.0
+
+   # 追加
+   config.time_zone = 'Tokyo'
+   config.i18n.default_locale = :ja
+  end
+end
